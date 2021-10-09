b1b6166d9077c0d7745995abdd3d3ddd9629ae31
bandgarmamta <bandgarmamta@gmail.com>
2021-10-09 19:34:28 +0530

    feat: url function

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ var textInput = document.querySelector("#txt-input");
 var inputUser = document.querySelectorAll(".input-user");
 var outputelement = document.querySelector("#output");
 var btnSubmit = document.querySelector("#btn-submit");
+
+var serverURL = "https://www.alphavantage.co/";
+textInput.addEventListener("input", clickHandler);
+
+function getStockURL(inputText) {
+  return serverURL + "query?function=SYMBOL_SEARCH&keywords=" + inputText + "&apikey=CK866LJ9T8I0VK2N";
+}
+
+function getCurrentURL(stockSymbol) {
+  return serverURL + "query?function=TIME_SERIES_DAILY&symbol=" + stockSymbol + "&apikey=CK866LJ9T8I0VK2N";
+}
