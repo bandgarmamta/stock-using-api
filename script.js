599a2adc38ead0b518fd86ec40c1c6ac447949c6
bandgarmamta <bandgarmamta@gmail.com>
2021-10-09 19:32:05 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+var textInput = document.querySelector("#txt-input");
+var inputUser = document.querySelectorAll(".input-user");
+var outputelement = document.querySelector("#output");
+var btnSubmit = document.querySelector("#btn-submit");
